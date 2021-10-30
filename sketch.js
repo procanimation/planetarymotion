f4544b7ec073f5815306a6d4618b500f3823fe33
root <shubhamshandilya198@gmail.com>
2021-10-30 14:29:37 +0530

    Planet file added

--- sketch.js
+++ sketch.js
@@ -1,14 +1,16 @@
-let earth;
+let earthtexture;
 let suntexture;
 let sun;
+let earth;
 let xoff;
 function preload(){
-   earth = loadImage('./images/earth.jpg');
+   earthtexture = loadImage('./images/earth.jpg');
    suntexture = loadImage('./images/sun.jpg');  
 }
 function setup() {
    createCanvas(640,640, WEBGL);
    sun = new Star(100, suntexture);
+   earth = new Planet(40, earthtexture);
    xoff = 0;
 }
 function draw(){
@@ -16,15 +18,13 @@ function draw(){
    //rotateY(xoff);
    sun.show();
 
-
-   let rotX = map(sin(xoff), -1, 1, -360, 360);
-   let rotY = map(sin(xoff), -1, 1, -60, 60);
+   // let rotX = map(sin(xoff), -1, 1, -360, 360);
+   // let rotY = map(sin(xoff), -1, 1, -60, 60);
    let rotZ = map(sin(xoff), -1, 1, -160, 160);
    rotateY(xoff);
+   translate(200, -100, rotZ);
+   earth.show();
    
-   translate(200, -100, rotZ)
-   texture(earth);
-   sphere(60);
    xoff += 0.01;
 
 }
