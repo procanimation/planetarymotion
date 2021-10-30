0cadd6fac42108892d0a3a6d7f0fa967289c222d
root <shubhamshandilya198@gmail.com>
2021-10-30 12:19:07 +0530

    Added file for star

--- sketch.js
+++ sketch.js
@@ -1,29 +1,31 @@
 let earth;
+let suntexture;
 let sun;
 let xoff;
 function preload(){
    earth = loadImage('./images/earth.jpg');
-   sun = loadImage('./images/sun.jpg');  
+   suntexture = loadImage('./images/sun.jpg');  
 }
 function setup() {
    createCanvas(640,640, WEBGL);
+   sun = new Star(100, suntexture);
    xoff = 0;
 }
 function draw(){
    background(51);
    //rotateY(xoff);
-   let light = new p5.Vector(0, 0, 0);
-   light.normalize()
-   pointLight(255, 255, 255, light)
-   texture(sun);
-   sphere(60);
+   sun.show();
+
+
    let rotX = map(sin(xoff), -1, 1, -360, 360);
    let rotY = map(sin(xoff), -1, 1, -60, 60);
    let rotZ = map(sin(xoff), -1, 1, -160, 160);
    rotateY(xoff);
-   translate(200, 0, rotZ)
+   
+   translate(200, -100, rotZ)
    texture(earth);
    sphere(60);
 
    xoff += 0.01;
+
 }
